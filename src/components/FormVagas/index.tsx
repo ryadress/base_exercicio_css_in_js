73adaabd1da3c819184067ea62ea42332cfd9ca1
Ryan silva <ryanrocke2004@gmail.com>
2024-09-18 20:25:56 -0300

    ajuste na estruturação do projeto

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,29 +1,27 @@
-import styled from 'styled-components';
+import { FormEvent, useState } from 'react';
+import { Formulario, BotaoPesquisar, Campo } from './styled';
 
-const Formulario = styled.form`
-  display: grid;
-  grid-template-columns: 1fr auto;
-  background-color: var(--cor-secundaria);
-  padding: 32px;
-  border-radius: 12px;
-  margin-top: 40px;
-`;
+type Props = {
+  aoPesquisar: (termo: string) => void;
+};
 
-const BotaoPesquisar = styled.button`
-  background-color: var(--cor-principal);
-  border: 1px solid var(--cor-principal);
-  height: 40px;
-  padding: 0 16px;
-  font-size: 18px;
-  color: var(--cor-secundaria);
-  margin-left: 8px;
-  cursor: pointer;
-`;
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('');
 
-const Campo = styled.input`
-  padding: 0 16px;
-  outline-color: var(--cor-principal);
-`;
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    aoPesquisar(termo.toLowerCase());
+  };
 
-export { Formulario, BotaoPesquisar, Campo };
+  return (
+    <Formulario onSubmit={aoEnviarForm}>
+      <Campo
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <BotaoPesquisar type="submit">Pesquisar</BotaoPesquisar>
+    </Formulario>
+  );
+};
 
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,44 +1,14 @@
-import styled from 'styled-components';
+import React from 'react';
+import { Hero, HeroTitle } from './styled';
 
-const Hero = styled.div`
-  height: 360px;
-  width: 100%;
-  background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
-  background-size: cover;
-  position: relative;
-  display: flex;
-  align-items: center;
+const HeroComponent: React.FC = () => (
+  <Hero>
+    <div>
+      <HeroTitle>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTitle>
+    </div>
+  </Hero>
+);
 
-  &::before {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: var(--cor-principal);
-    content: '';
-    opacity: 0.7;
-  }
-
-  div {
-    position: relative;
-    color: #eee;
-  }
-
-  @media (max-width: 768px) {
-    height: auto;
-    padding: 24px 0;
-  }
-`;
-
-const HeroTitle = styled.h1`
-  font-family: Gloock, serif;
-  font-size: 48px;
-
-  @media (max-width: 768px) {
-    font-size: 32px;
-  }
-`;
-
-export { Hero, HeroTitle };
-
+export default HeroComponent;
